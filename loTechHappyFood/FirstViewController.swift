3f91902e286c054a5cda74eef5bdd39ff2b981a7
kate <ekaterina.roberts@gmail.com>
2019-04-23 13:45:54 +0100

    Initial Commit

---
+++ loTechHappyFood/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  loTechHappyFood
+//
+//  Created by Kate Roberts on 23/04/2019.
+//  Copyright © 2019 Kate Roberts. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ loTechHappyFood/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  loTechHappyFood
+//
+//  Created by Kate Roberts on 23/04/2019.
+//  Copyright © 2019 Kate Roberts. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
